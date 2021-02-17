f5da267a1e4f3fde56cc894b35b79fad1e783889
ChinonsoIg <ojononso@yahoo.com>
2021-02-17 11:44:40 +0100

    edited home page

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 You can get the exchange rates of different currencies, by providing your home currency.
 By default, the home currency is EUR. This can be changed by providing a query parameter named base. 
-You can as well get exchange rate for specific currencies by providing query parameter named currency. The currency query parameter can accept multiple values.
-The 
+You can as well get exchange rate for specific currencies by providing query parameter named currency. The currency query parameter can accept multiple values, separated with commas.
 
 The default endpoint is:
+```
 /api/rates
+```
 
-To add query parameters
+To add query parameters:
+```
 /api/rates?base=CAD&currency=GBP,HKD,ISK,PHP
+```
+will produce the data like so:
+```
+"results": {
+  "base": "CAD",
+  "date": "2021-02-16",
+  "rates": {
+    "GBP": 0.87333,
+    "HKD": 9.4139,
+    "ISK": 155.6,
+    "PHP": 58.606
+  }
+}
+```
--- app.js
+++ app.js
@@ -5,55 +5,11 @@ const exchangeRouter = require('./routes/exchangeRoutes');
 const app = express();
 const port = process.env.PORT || 4001;
 
-// exchangeRouter.route(`/rates`)
-//   .get((req, res) => {
-//     const { base, currency } = req.query;
-    
-//     apiCallFromRequest.callApi(function(response) {
-
-//       // If there is no query parameter
-//       if (!base && !currency) {
-//         return res.status(200).json({
-//           results: {
-//             "base": response.base,
-//             "date": response.date,
-//             "rates": response.rates
-//           }
-//         })
-//       }
-
-//       // If there is a query parameter but no value
-//       if (!base) {
-//         return res.status(400).json({ message: "Please specify your home currency" })
-//       }
-//       if (!currency) {
-//         return res.status(400).json({ message: "Please specify exchange currency or currencies" })
-//       }
-
-//       // If there is query parameter and a value
-//       const exchangeRates = currency.toUpperCase().split(',')
-//       let currencyExchangeRates = {};
-//       for (const exchangeRate of exchangeRates) {
-//         //trim white space
-//         const value = exchangeRate.trim();
-//         currencyExchangeRates = { ...currencyExchangeRates, [value]: response.rates[value] }
-//       }
-//       console.log(currencyExchangeRates);
-//       res.json({
-//         results: {
-//           "base": base,
-//           "date": response.date,
-//           "rates": currencyExchangeRates
-//         }
-//       })
-//       res.end();
-//     });
-//   });
 
 app.use('/api', exchangeRouter);
 
 app.get('/', (req, res) => {
-  res.send(`Welcome to my nodemon API. Visit this link '/api/rates'`)
+  res.send(`Welcome to my Node API. Add '/api/rates' to the link to fetch all rates`)
 });
 
 
